c895fb58f4be99dbd059a0da8c9935eef6d37b01
muammarnibros <muammarnibros027@students.amikom.ac.id>
2025-07-14 21:44:58 +0700

    update mode mobile3

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,7 @@
 import { RouterView } from 'vue-router';
 </script>
 <template>
-<div class=" container mx-auto font-sans relative bg-wallpaper bg-cover h-screen bg-center  text-white">
+<div class="mx-auto font-sans relative bg-wallpaper bg-cover h-screen bg-center  text-white">
     <Navbar />
     <main class="min-h-screen">
         <RouterView />
